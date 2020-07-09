d29bdb29fac53f5bdeb5d4591755937c7a63d687
Trevor Martin <trevor.j.martin@gmail.com>
2020-07-09 01:00:29 -0400

    introduction

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { faCat } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { NavLink } from "react-router-dom";
+
 import "./home.css";
 const Home = (props) => {
   return (
@@ -10,6 +12,26 @@ const Home = (props) => {
       </h1>
       <FontAwesomeIcon icon={faCat} />
       {props.text}
+      <div className="hello-world">
+        <h1>Hello, I'm Trevor Martin.</h1>
+        <h2>I'm a full stack web engineer</h2>
+        <br />
+        <br />
+        <ul>
+          <li>
+            <NavLink style={{ letterSpacing: "1px" }} to="/projects">
+              Why not peruse some open source projects I've worked on.
+            </NavLink>{" "}
+          </li>
+          <br />
+          <br />
+          <li>
+            <NavLink style={{ letterSpacing: "1px" }} to="/about">
+              Or find out more about me.
+            </NavLink>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,6 +2,14 @@ import React from "react";
 import { animated } from "react-spring";
 import "./modal.css";
 
+/**
+ *
+ * @param {*} style the style
+ * @param {Function} closeModal function that closes the dialog box
+ * @param {string} title dialog title
+ * @param {string} text dialog text
+ * @param {Function} faIcon function that returns a FontAwesome icon
+ */
 const Modal = ({ style, closeModal, title, text, faIcon }) => (
   <animated.div style={style} className="modal">
     <h2>{faIcon()}</h2>
