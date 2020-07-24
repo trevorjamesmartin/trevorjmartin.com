f633ecb7d21b737bc862b41d8a5f6dcce5105ad1
Trevor Martin <trevor.j.martin@gmail.com>
2020-07-24 14:53:44 -0400

    moved cards to nifty-components

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { faCat } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -8,29 +8,10 @@ import {
   faHtml5,
   faLinux,
 } from "@fortawesome/free-brands-svg-icons";
-// import "./projects.css";
-// import { Card } from "../Card";
-import { ListCard, ListCardTable } from "../Card/ListCard";
+import { GithubCards } from "nifty-components";
+
 import projectList from "./projects.json";
 const Projects = (props) => {
-  const [selected, setSelected] = useState({
-    key: undefined,
-    opened: undefined,
-  });
-  const selectCard = (key) => {
-    selected.opened === undefined && setSelected({ key });
-  };
-  const toggleOpen = (key) => {
-    if (selected.key === key) {
-      if (selected.opened === key) {
-        setSelected({ key: undefined, opened: undefined }); // close card
-      } else {
-        setSelected({ ...selected, opened: key }); // select and open
-      }
-    }
-  };
-
-  // console.log(cards);
   return (
     <div className="page">
       <h1>
@@ -42,46 +23,7 @@ const Projects = (props) => {
       </h1>
       <FontAwesomeIcon icon={faCat} />
       {props.text}
-      <ListCardTable
-        style={{
-          maxWidth: "42rem",
-          display: "flex",
-          flexDirection: "column",
-          margin: "2ch",
-        }}
-        cards={projectList.map(
-          (
-            {
-              title,
-              thumb,
-              alt,
-              desc: briefdesc,
-              hosted: hostedURL,
-              source: sourceURL,
-              portrait,
-              tldr,
-            },
-            key
-          ) =>
-            ListCard({
-              key: key + 1,
-              title,
-              briefdesc,
-              cardtags: alt,
-              avatar: thumb,
-              tldr,
-              toggleOpen: () => toggleOpen(key + 1),
-              selectCard: () => selectCard(key + 1),
-              isSelected: selected.key === key + 1,
-              isOpen: selected.opened === key + 1,
-              cardNumber: key + 1,
-              palette: props.palette,
-              hostedURL,
-              sourceURL,
-              portrait,
-            })
-        )}
-      />
+      <GithubCards projectlist={projectList} palette={props.palette} />
     </div>
   );
 };
